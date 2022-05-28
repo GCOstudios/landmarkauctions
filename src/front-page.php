a45756af26199eba3dce1cdbd9a1058a24c55724
jsguerra <jose@gcostudios.com>
2022-05-28 09:28:46 +0100

    initial commit

---
+++ src/front-page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+  <main>
+    <div>
+      <h1>Hero Banner</h1>
+    </div>
+    <div>
+      <p>Three Cards: Auction guild, Sell Now, Valuations</p>
+    </div>
+    <div>
+      <p>Section with image and CTA</p>
+    </div>
+    <div>
+      <p>Landmark Auctions - are focused<br>on selling property nationwide.</p>
+    </div>
+    <div>
+      <p>Image section</p>
+    </div>
+    <div>
+      <p>Four cards: Register with us, FAQ's, Meet the team, Contact us</p>
+    </div>
+    <div>
+      <p>Carousel with quotes</p>ß
+    </div>
+  </main>
+<?php
+get_footer();
