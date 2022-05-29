e218c98791b83ada29b7eb21868a5ac2d22a5113
jsguerra <jose@gcostudios.com>
2022-05-29 13:51:03 +0100

    initial commit

---
+++ src/page-home.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Homepage
+ * description: Custom Homepage Layout
+ */
+get_header(); ?>
+  <main>
+    <div class="hero">
+      <?php echo do_shortcode('[slick-slider arrows="false"]'); ?>
+      <div class="hero-info">
+        <h2>Sell or Buy a property</h2>
+        <p>For more information and enquiries, contact<br>us for some friendly no fee advice.</p>
+        <p>07974 179 607</p>
+      </div>
+    </div>
+    
+    <div>
+      <p>Three Cards: Auction guild, Sell Now, Valuations</p>
+    </div>
+    <div>
+      <p>Section with image and CTA</p>
+    </div>
+    <div>
+      <p>Landmark Auctions - are focused<br>on selling property nationwide.</p>
+    </div>
+    <div>
+      <p>Image section</p>
+    </div>
+    <div>
+      <p>Four cards: Register with us, FAQ's, Meet the team, Contact us</p>
+    </div>
+    <div>
+      <p>Carousel with quotes</p>ß
+    </div>
+  </main>
+<?php
+get_footer();
