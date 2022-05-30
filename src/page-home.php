8dd0a4b0d628b151c8cddb70ebb34f43cc0b8370
jsguerra <jose@gcostudios.com>
2022-05-30 20:29:37 +0100

    updated sections

--- src/page-home.php
+++ src/page-home.php
@@ -20,25 +20,50 @@ get_header(); ?>
         $secitonOne = get_field('section_one');
         if($secitonOne):?>
           <div class="card">
-            <?php echo $secitonOne['auction_guide'] ?>
+            <?php echo $secitonOne['auction_guide']; ?>
           </div>
           <div class="card">
-            <?php echo $secitonOne['sell_now'] ?>
+            <?php echo $secitonOne['sell_now']; ?>
           </div>
           <div class="card">
-            <?php echo $secitonOne['valuations'] ?>
+            <?php echo $secitonOne['valuations']; ?>
           </div>
         <?php endif; ?>
       </div>
     </div>
-    <div>
-      <p>Section with image and CTA</p>
+    <div class="section-two">
+      <?php
+        $secitonTwo = get_field('section_two');
+        if($secitonTwo):?>
+          <div class="section-image">
+            <img src="<?php echo esc_url( $secitonTwo['image']['url'] ); ?>" alt="<?php echo esc_attr( $secitonTwo['image']['alt'] ); ?>" />
+          </div>
+          <div class="seciton-info">
+            <div class="section-container">
+              <?php echo $secitonTwo['online_auctions']; ?>
+            </div>
+          </div>
+        <?php endif; ?>
     </div>
-    <div>
-      <p>Landmark Auctions - are focused<br>on selling property nationwide.</p>
+    <div class="section-three">
+      <?php if( get_field('section_three') ): ?>
+        <div class="section-quote">
+          <?php the_field('section_three'); ?>
+        </div>
+      <?php endif; ?>
     </div>
-    <div>
-      <p>Image section</p>
+    <div class="section-four">
+      <?php
+        $secitonFour = get_field('section_four');
+        if($secitonFour):?>
+          <div style="background-image: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('<?php echo esc_url( $secitonFour['background_image']['url'] ); ?>');" class="section-info">
+            <div class="section-container">
+              <div class="section-entry">
+                <?php echo $secitonFour['why_landmark']; ?>
+              </div>
+            </div>
+          </div>
+        <?php endif; ?>
     </div>
     <div>
       <p>Four cards: Register with us, FAQ's, Meet the team, Contact us</p>
