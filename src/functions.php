9b113867632f9f1632cdd04d12a5b8110b7b09e5
jsguerra <jose@gcostudios.com>
2022-06-02 20:08:39 +0100

    registered sidebars

--- src/functions.php
+++ src/functions.php
@@ -131,6 +131,72 @@ function landmarkauctions_widgets_init() {
 			'after_title'   => '</h2>',
 		)
 	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Address', 'landmarkauctions' ),
+			'id'            => 'footer-1',
+			'description'   => esc_html__( 'Add widgets here.', 'landmarkauctions' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Links', 'landmarkauctions' ),
+			'id'            => 'footer-2',
+			'description'   => esc_html__( 'Add widgets here.', 'landmarkauctions' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Partners', 'landmarkauctions' ),
+			'id'            => 'footer-3',
+			'description'   => esc_html__( 'Add widgets here.', 'landmarkauctions' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Connect', 'landmarkauctions' ),
+			'id'            => 'footer-4',
+			'description'   => esc_html__( 'Add widgets here.', 'landmarkauctions' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Phone', 'landmarkauctions' ),
+			'id'            => 'footer-5',
+			'description'   => esc_html__( 'Add widgets here.', 'landmarkauctions' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Logos', 'landmarkauctions' ),
+			'id'            => 'footer-6',
+			'description'   => esc_html__( 'Add widgets here.', 'landmarkauctions' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 }
 add_action( 'widgets_init', 'landmarkauctions_widgets_init' );
 
