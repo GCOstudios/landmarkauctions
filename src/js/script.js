233aee3d8246d67449bea074d50d9f71b2116e52
jsguerra <jose@gcostudios.com>
2022-06-08 21:04:22 +0100

    added aos js script

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,65 @@
-const hello = 'hello world';
+jQuery(function($) {
 
-console.log(hello);
+  'use strict';
 
-console.log('another message ok');
+  var ENHANCEMENTS;
+
+  // ================================
+  // Enhancements
+  // ================================
+
+    ENHANCEMENTS = {
+
+    init: function() {
+      this.animations();
+    },
+
+    // AOS animations
+    // ==============================================
+    animations: function() {
+      // media query event handler
+      if (matchMedia) {
+        var mq = window.matchMedia("(min-width: 600px)");
+
+        mq.addListener(WidthChange);
+        WidthChange(mq);
+      }
+
+      // media query change
+      function WidthChange(mq) {
+        if (mq.matches) {
+          // window width is at least 600px
+          $('.fade-up').attr('data-aos', 'fade-up');
+          $('.fade-right').attr('data-aos', 'fade-right');
+          $('.fade-down').attr('data-aos', 'fade-down');
+          $('.fade-left').attr('data-aos', 'fade-left');
+          $('.fade-in').attr('data-aos', 'fade-in');
+
+          $('.box-item').each(function(index) {
+            var $this = $(this);
+            $this.attr('data-aos', 'fade-up').attr('data-aos-delay', ((index + 1) * 4) + '00');
+          });
+
+          $('.box-item-up').each(function(index) {
+            var $this = $(this);
+            $this.attr('data-aos', 'fade-up').attr('data-aos-delay', ((index + 1) * 4) + '00');
+          });
+        } else {
+          // window width is less than 600px
+        }
+      }
+
+      setTimeout(function() {
+        AOS.init({
+          offset: 200,
+          duration: 800,
+          delay: 100,
+        });
+      }, 300);
+    }
+
+  };
+  
+  ENHANCEMENTS.init();
+
+}); //jQuery
