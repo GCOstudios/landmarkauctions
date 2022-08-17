356f2e063bdd71f1cdc339929149ab283fdc70f3
jsguerra <jose@gcostudios.com>
2022-08-17 22:08:03 +0100

    file updated

--- src/page-property.php
+++ src/page-property.php
@@ -8,11 +8,17 @@ get_header(); ?>
 
 <main id="primary" class="site-main">
   <article id="post-<?php the_ID(); ?>" <?php post_class('container'); ?> itemtype="https://schema.org/WebPage" itemscope="itemscope">
+    <?php
+      $auction_root = wpgetapi_endpoint( 'ams1', 'auction_root', array('debug' => false) );
+      $property_id = $_GET['propertyId'];
+      echo print_r($auction_root);
+    ?>
     <header>
-      <h1></h1>
+      <h1><?php echo $_GET['propertyId']; ?></h1>
     </header>
     <div class="auction-grid">
-
+      <?php // print_r($_GET); ?>
+      <?php print_r($auction_root.'"/getauctiondetails/"'.$property_id); ?>
     </div>
   </article>
 </main>
