9aa773f4d3ab2c605e97a9b50dbf0333f047f32e
jsguerra <jose@gcostudios.com>
2023-02-01 00:13:15 +0000

    added filter logic

--- src/page-auctions.php
+++ src/page-auctions.php
@@ -4,7 +4,37 @@
  * Template Name: Auction Listing
  */
 
-get_header(); ?>
+get_header();
+
+$auctions = wpgetapi_endpoint( 'ams1', 'auctions', array('debug' => false) );
+// $propteries = json_decode($auctions);
+// var_dump($auctions);
+$i = 1;
+
+$filteredOnWeb = array_filter(
+  $auctions,
+  function($obj) {
+    return $obj['IsShownOnWeb'] === true;
+  }
+);
+
+$filteredFutureAuction = array_filter(
+  $filteredOnWeb,
+  function($obj) {
+    $EndDate = $obj['EndDate'];
+    $futureDate = strtotime($EndDate) - time();
+    return  $futureDate > 0;
+  }
+);
+
+// var_dump($filteredFutureAuction);
+// var_dump($currentTime);
+
+// foreach ($auctions as $value) {
+//   echo "<pre>"; var_dump($value);
+// }
+
+?>
 
 <main id="primary" class="site-main">
   <article id="post-<?php the_ID(); ?>" <?php post_class('container'); ?> itemtype="https://schema.org/WebPage" itemscope="itemscope">
@@ -13,22 +43,13 @@ get_header(); ?>
     </header>
     <hr>
     <div class="grid-filters">
-      <p>Showing results 1 - 12 of 12</p>
+      <p>Showing results <?php echo count($filteredFutureAuction); ?></p>
     </div>
     <div class="auction-grid">
     <?php // do our api calls
-      $auctions = wpgetapi_endpoint( 'ams1', 'auctions', array('debug' => false) );
-      // $propteries = json_decode($auctions);
-      // var_dump($auctions);
-      $i = 1;
-
-      // foreach ($auctions as $value) {
-      //   echo "<pre>"; var_dump($value);
-      // }
-
-      foreach ($auctions as $value => $val) {
-        $img_url = $auctions[$value]['Thumbnail'];
-        $start_date = $auctions[$value]['StartDate'];
+      foreach ($filteredFutureAuction as $value => $val) {
+        $img_url = $filteredFutureAuction[$value]['Thumbnail'];
+        $start_date = $filteredFutureAuction[$value]['StartDate'];
 
         // echo $start_date;
         // echo date('YW', strtotime($start_date));
@@ -36,19 +57,19 @@ get_header(); ?>
         if ($img_url == null) {
           $img_url = "https://via.placeholder.com/255x218";
         }
-        echo '<a href="/property-details?lotId='. $auctions[$value]['LotID'] .'">';
+        echo '<a href="/property-details?lotId='. $filteredFutureAuction[$value]['LotID'] .'">';
         echo '<div class="property-card">';
           echo '<div style="background-image: url('. $img_url .');" class="grid-img"></div>';
           echo '<div class="grid-info">';
-            echo '<h4 class="grid-address">'. $auctions[$value]['Tagline'] .'</h4>';
+            echo '<h4 class="grid-address">'. $filteredFutureAuction[$value]['Tagline'] .'</h4>';
             echo '<hr>';
             echo '<div class="lot-info">';
               echo '<ul class="list-unstyled">';
-                echo '<li><span class="primary-text">Status</span> '. $auctions[$value]['SoldStatus'] . '</li>';
+                echo '<li><span class="primary-text">Status</span> '. $filteredFutureAuction[$value]['SoldStatus'] . '</li>';
                 echo '<li><span class="primary-text">Auction</span> '.date('d/m/Y H:i', strtotime($start_date)). '</li>';
                 echo '<li><span class="primary-text">Lot</span> </li>';
                 echo '<li><span class="primary-text">Sector</span> </li>';
-                echo '<li><span class="primary-text">Price</span> Guide Price*:&nbsp;&nbsp;<strong>&pound;'. $auctions[$value]['GuidePrice']. '</strong></li>';
+                echo '<li><span class="primary-text">Price</span> Guide Price*:&nbsp;&nbsp;<strong>&pound;'. $filteredFutureAuction[$value]['GuidePrice']. '</strong></li>';
                 echo '<li><span class="primary-text">Yield</span> </li>';
               echo '</ul>';
             echo '</div>';
@@ -65,7 +86,7 @@ get_header(); ?>
       <p class="search-page-disclaimer">* Generally speaking Guide Prices are provided as an indication of each seller's minimum expectation, i.e. 'The Reserve'. They are not necessarily figures which a property will sell for and may change at any time prior to the auction. Virtually every property will be offered subject to a Reserve (a figure below which the Auctioneer cannot sell the property during the auction) which we expect will be set within the Guide Range or no more than 10% above a single figure Guide.</p>
     </div>
     <!-- <?php esc_html_e( $propteries[1]->Reference); ?> -->
-    <!-- <?php esc_html_e( $auctions[0]['FullAddress']); ?> -->
+    <!-- <?php esc_html_e( $filteredFutureAuction[0]['FullAddress']); ?> -->
   </article>
 </main>
 
